7dcd07691688932beb2faf59bc7e6940f49d2eb4
Brian Nkuru <nkurunungib@gmail.com>
2020-12-27 23:18:29 -0600

    Config changes

--- product-service/src/main/java/com/crystal/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/crystal/productservice/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.crystal.productservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+   // @Value("${dbsetting}")
+    @Value("dbsetting")
+    private String dbString;
 
     @GetMapping("/")
     public String display(){
         System.out.println("It workds ");
-        return "It works";
+        return "It works"+dbString;
     }
 }
